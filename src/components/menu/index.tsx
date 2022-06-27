fccd10507ca082ad56c128d28bc65464b1591830
Lucas P. Sodré <llucasbrasill@gmail.com>
2022-06-27 16:23:35 -0300

    feat: add image and url links

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import styles from './styles.module.css'
+import icoBusinessSchool from '../../assets/images/icoBusinessSchool.svg'
+import icoFamilySchool from '../../assets/images/icoFamilySchool.svg'
+import icoGerminaTECH from '../../assets/images/icoGerminaTECH.svg'
+import IcoCommunityGerminare from '../../assets/images/IcoCommunityGerminare.svg'
 
 const Menu: React.FC = () => {
+
     return <>
         <nav className={styles.wrapper}>
             <ul>
-                <li><a href='http://brasfirst.com/'><img src="/assets/images/icoBusinessSchool.svg" alt="Germinare - Escola de Négocios"  /></a></li>
-                <li><a href='http://brasfirst.com/'><img src="/assets/images/icoFamilySchool.svg" alt="Germinare - Escola da Família"  /></a></li>
-                <li><a href='http://brasfirst.com/'><img src="/assets/images/icoGerminaTECH.svg" alt="GerminaTECH"  /></a></li>
-                <li><a href='http://brasfirst.com/'><img src="/assets/images/IcoComunityGerminare.svg" alt="Comunidade Germinare"  /></a></li>
+                <li><a href={process.env.REACT_APP_LINK_1}><img src={icoBusinessSchool} alt="Germinare - Escola de Négocios" /></a></li>
+                <li><a href={process.env.REACT_APP_LINK_2}><img src={icoFamilySchool} alt="Germinare - Escola da Família" /></a></li>
+                <li><a href={process.env.REACT_APP_LINK_3}><img src={icoGerminaTECH} alt="GerminaTECH" /></a></li>
+                <li><a href={process.env.REACT_APP_LINK_4}><img src={IcoCommunityGerminare} alt="Comunidade Germinare" /></a></li>
             </ul>
         </nav>
     </>
